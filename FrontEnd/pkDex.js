e42182eee969210e11df62f92f6ddb5b7fb6e3fd
craig2812 <craig2812@gmail.com>
2021-03-03 13:03:31 +0000

    j

--- FrontEnd/pkDex.js
+++ FrontEnd/pkDex.js
@@ -1,11 +1,18 @@
-//GET QUESTION FUNCTION//
-async function getQuestions(){
-    // startButton.innerText = "Restart Quiz" // need this to refresh page not just start a new question
-    
-    // create random number 
-    let rndnum = Math.random();
-    // insert random number into path 
-    let response = await fetch (`https://localhost:5001/pkms/14`);
-    let data = await response.json();
-    console.log(data);
+//GET PKM FUNCTION//
+async function getPkm(){    
+
+    // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const url = "https://localhost:5001/pkms"; // site that doesn’t send Access-Control-*
+let response = await fetch (url) // https://cors-anywhere.herokuapp.com/https://example.com
+let data = await response.json();
+console.log(data);
+
+
+    // // create random number 
+    // let rndnum = Math.random();
+    // // insert random number into path 
+    // let response = await fetch (`https://localhost:5001/pkms`); //correct endpoint - endpoint just being the server address not including route 
+    // let data = await response.json();
+    // console.log(data);
 }
+// getQuestions()
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
 
 namespace src
 {
@@ -19,6 +29,7 @@ namespace src
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddTransient<>();
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(
@@ -40,18 +51,17 @@ namespace src
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "src v1"));
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "src v1"));
+            // }
 
             app.UseRouting();
-
             app.UseCors();
 
-            app.UseAuthorization();
+            // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
